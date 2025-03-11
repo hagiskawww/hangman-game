5e3911dfef53c8e59c0247c8db3a1e6df8987aef
Vlad <v.zhurabetmen@gmail.com>
2025-03-11 11:46:42 +0200

    add vite config js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import postcss from 'postcss'
 
 export default defineConfig({
+    base: '/hangman-game/',
     plugins: [
         tailwindcss(),
         postcss(),
